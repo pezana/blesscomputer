1b5a7174c181090599b2accaf6824c7dd51e6c9a
pezana <blondokam2@gmail.com>
2022-08-08 16:40:12 +0200

    ajoute de la fenetre appro.html et de sa fiche de vue

--- prod/urls.py
+++ prod/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
+from . import approv
 
 urlpatterns = [
-    path('',views.acceuil,name='acceuil'),    
+    path('',views.acceuil,name='acceuil'),
+    path('/Approvisionnement',approv.redirectionapprov,name='approvLien')    
 ]
