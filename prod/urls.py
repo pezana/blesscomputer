5b5c6168b37d9f715e8b99e3293f7b2d4b3300d6
FranckFotso <franckulrich16@gmail.com>
2022-08-09 14:06:42 +0100

    modification base.html

--- prod/urls.py
+++ prod/urls.py
@@ -2,11 +2,14 @@ from django.urls import path,include
 from . import views
 from . import approv
 from . import production
+from . import fournisseur
 
 
 
 urlpatterns = [
     path('',views.acceuil,name='acceuil'),
     path('Approvisionnement',approv.redirectionapprov,name='approvLien'), 
-    path('Production',production.redirectionproduction,name='prodLien') ,  
+    path('Production',production.redirectionproduction,name='prodLien') ,
+    path('Fournisseur',fournisseur.redirectionfournisseur,name='fournisseurLien') ,  
+
 ]
