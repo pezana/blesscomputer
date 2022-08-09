201a77a8a8dd4163e9eae91184833c3d91b04ea9
pezana <blondokam2@gmail.com>
2022-08-09 13:10:15 +0200

    maj de admin.py

--- prod/urls.py
+++ prod/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 from . import approv
 
+
 urlpatterns = [
     path('',views.acceuil,name='acceuil'),
     path('Approvisionnement',approv.redirectionapprov,name='approvLien')    
