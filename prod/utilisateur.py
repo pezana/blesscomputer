a786f2d9d9cfbeefde4771768a1cb5bbbc319312
dylane2 <dylankuo75@gmail.com>
2022-08-12 10:39:29 +0200

    configuration de la fenetre utilisateur

--- prod/utilisateur.py
+++ prod/utilisateur.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import *
-from django import forms
 from django.shortcuts import get_object_or_404
-
+from django import forms
 
 def redirectionutilisateur(request):
      listeutilisateur=utilisateur.objects.all()
@@ -11,4 +10,31 @@ def redirectionutilisateur(request):
 
 def voirUtilisateur(request, id):
      voir_Utilisateur = get_object_or_404(utilisateur, id = id)
-     return render(request,'detailUtilisateur.html', {'monutilisateur': voir_Utilisateur})     
+     return render(request,'detailUtilisateur.html', {'monutilisateur': voir_Utilisateur})   
+
+
+
+class ajoututilisateurform(forms.Form):     
+      profil=forms.CharField(label='nom utilisateur')
+      pseudo=forms.CharField(required=True)
+      mdepass=forms.CharField(required=True)
+          
+#  la fonction qui permet d'ajouter les utilisateurs dans la base de donnees    
+def ajoututilisateur(request):
+     form=ajoututilisateurform()
+     if request.POST:
+          form=ajoututilisateurform(request.POST)  
+          if form.is_valid():  
+               prod= utilisateur.objects.create(**form.cleaned_data)                            
+               prod.save()  
+               form=ajoututilisateurform() 
+               return render(request,'ajouterutilisateur.html',{'form':form,'msg':'ajout utilisateur avec succès'}) 
+          else :
+               form=ajoututilisateurform() 
+               return render(request,'ajouterutilisateur.html',{'form':form,'msg':'vos information sont invalides'})
+     else :
+          form=ajoututilisateurform()
+          return render(request,'ajouterutilisateur.html',{'form':form})               
+
+def utilisateurjouter(request):
+     return render(request,'ajouterutilisateur.html')
