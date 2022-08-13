64f221363e266df06a967affe1e3b6b0216a02e7
Your Name <you@example.com>
2022-08-13 18:27:07 +0100

    configuration de la fenetre client

--- prod/client.py
+++ prod/client.py
@@ -12,11 +12,27 @@ def detailclient(request, id):
     voir_client= get_object_or_404(client, id= id)
     return render(request,'detailclient.html',{'maliste': voir_client})
 
-class ajoutclientform(forms.Form):     
-      nom=forms.CharField(label='nom du client',required=True)
-      adress=forms.CharField()
-      tel=forms.CharField()
-      typeclient=forms.CharField()
+class ajoutclientform(forms.Form):
+     nom=forms.CharField(label='',required=True, widget=forms.TextInput( 
+          attrs={
+               'PlaceHolder': 'nom du client', 
+
+          }))
+     adress=forms.CharField(label='',required=True, widget=forms.TextInput( 
+          attrs={
+               'PlaceHolder': 'adresse du client',
+
+          }))
+     tel=forms.CharField(label='',required=True, widget=forms.TextInput( 
+          attrs={
+               'PlaceHolder': 'telephone',     
+
+          }))
+     typeclient=forms.CharField(label='',required=True, widget=forms.TextInput( 
+          attrs={
+               'PlaceHolder': 'type de client',
+          }))          
+
           
      
 def ajoutclient(request):
